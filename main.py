629d470e29d7b1de66aac2f2e9bd74abc12fa93f
Mabi671 <karol.wladzinski@gmail.com>
2024-06-15 17:49:20 +0200

    15.06

--- main.py
+++ main.py
@@ -19,37 +19,49 @@ def draw():
     for rect in rects:
         rect.draw()
 
-def make_tunnel(n):
-    for i in range(n):
-        rect = Rect(250, 500 - (i * 55), 25, 50, False)
-        rects.append(rect)
-        rect = Rect(300, 500 - (i * 55), 25, 50, False)
-        rects.append(rect)
+width, height = 10, 10
+scale = 10.0
+seed = 42
+
+def generate_maze():    
+    for i in range(screen_width // 50):
+        for j in range(screen_height // 50):
+            rott = random.randint(0, 1)
+            rect = Rect(25 - 20 * rott + i * 50, rott * 20  + j * 50,10, 50, rott)
+            rects.append(rect)
 
 
 class Rect:
-    def __init__(self, x, y, width, height, ang):
+    def __init__(self, x, y, width, height, rot):
         self.x = x
         self.y = y
-        self.width = width - (width * ang) + (height * ang)
-        self.height = height - (height * ang) + (width * ang)
+        self.width = width - (width * rot) + (height * rot)
+        self.height = height - (height * rot) + (width * rot)
         self.rect = pygame.Rect(x , y, width, height)
         self.color = BLUE
+        self.rot = rot
+        self.rotate()
+        self.rotated_surface = pygame.Surface((self.width, self.height))
+    def rotate(self):
+        rect_surface = pygame.Surface((self.width, self.height))  
+        self.rotated_surface = pygame.transform.rotate(rect_surface, 90 * self.rot)
+        self.rect = self.rotated_surface.get_rect(center=(self.x, self.y))
     def draw(self):
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
-    
-    def upadateRect(self):
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.rotated_surface.fill(BLUE)
+        screen.blit(self.rotated_surface, self.rect.center)
 
 clock = pygame.time.Clock()
-make_tunnel(5)
+
 running = True
 while running:
-    clock.tick(2)
+    clock.tick(3)
     for event in pygame.event.get():            
         if event.type == pygame.QUIT:
             running = False
     draw()
+    rects = []
+    generate_maze()
+    
     pygame.display.flip()
 
 
