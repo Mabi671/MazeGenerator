6399d92c8fe6bf0ad5b545ebed7d7757a556bd40
Mabi671 <karol.wladzinski@gmail.com>
2024-06-17 21:35:05 +0200

    17.06

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pygame # type: ignore
 import time 
-import pygame
 import sys
 import random
 import math
 
-pygame.init()
+#pygame.init()
 
 screen_width, screen_height = 800, 600
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -20,22 +19,32 @@ def draw():
         rect.draw()
 
 
-def generate_maze():    
-    for i in range(screen_width // 50):
-        for j in range(screen_height // 50):
+def generate_maze():
+    max_i = screen_width // 50    
+    max_j = screen_height // 50
+    first_gate = random.randint(0, max_j - 1)
+    second_gate = random.randint(0, max_j - 1)
+    for i in range(max_i - 1):
+        for j in range(max_j):
             rott = random.randint(0, 1)
-            rect = Rect(25 - 20 * rott + i * 50, rott * 20  + j * 50,10, 50, rott)
+            if(i == 0 and first_gate == j or i == 0 and first_gate + 1 == j or i == max_i-2 and second_gate == j or i == max_i-2 and second_gate + 1 == j):
+                rott = 1
+                rect = Rect(25 - 20 * rott + i * 53, rott * 20  + j * 50,10, 70, rott, RED)
+                rects.append(rect)
+                continue
+            rect = Rect(25 - 20 * rott + i * 50, rott * 20  + j * 50,10, 70, rott, BLUE)
             rects.append(rect)
+            
 
 
 class Rect:
-    def __init__(self, x, y, width, height, rot):
+    def __init__(self, x, y, width, height, rot, color):
         self.x = x
         self.y = y
         self.width = width - (width * rot) + (height * rot)
         self.height = height - (height * rot) + (width * rot)
         self.rect = pygame.Rect(x , y, width, height)
-        self.color = BLUE
+        self.color = color
         self.rot = rot
         self.rotate()
         self.rotated_surface = pygame.Surface((self.width, self.height))
@@ -44,7 +53,7 @@ class Rect:
         self.rotated_surface = pygame.transform.rotate(rect_surface, 90 * self.rot)
         self.rect = self.rotated_surface.get_rect(center=(self.x, self.y))
     def draw(self):
-        self.rotated_surface.fill(BLUE)
+        self.rotated_surface.fill(self.color)
         screen.blit(self.rotated_surface, self.rect.center)
 
 clock = pygame.time.Clock()
